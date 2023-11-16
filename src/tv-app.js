219a74052bec5c1db1b62642d0fe24351a8efc9d
trip <daxton416@gmail.com>
2023-11-15 23:26:28 -0500

    Added initial features

--- src/tv-app.js
+++ src/tv-app.js
@@ -11,6 +11,13 @@ export class TvApp extends LitElement {
     this.name = '';
     this.source = new URL('../assets/channels.json', import.meta.url).href;
     this.listings = [];
+    this.activeItem = {
+      id: null,
+      title: null,
+      presenter: null,
+      description: null,
+      video: null
+    }
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
@@ -22,6 +29,7 @@ export class TvApp extends LitElement {
       name: { type: String },
       source: { type: String },
       listings: { type: Array },
+      activeItem: { type: Object }
     };
   }
   // LitElement convention for applying styles JUST to our element
@@ -30,8 +38,83 @@ export class TvApp extends LitElement {
       css`
       :host {
         display: block;
-        margin: 16px;
+      }
+
+      header {
+        color: #000;
         padding: 16px;
+        text-align: center;
+      }
+
+      .h1 {
+        font-size: 32px;
+        margin-bottom: 16px;
+      }
+
+      /* .channel-container {
+        justify-self: center;
+        max-width: 1344px;
+        justify-items: left;
+        display: flex;
+        flex-direction: row;
+        flex-grow: 1;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        overflow-y: auto;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        text-rendering: optimizeLegibility;
+        width: 100%;
+        margin: 0 auto;
+        position: relative;
+        animation-delay: 1s;
+        animation-duration: 1s;
+        line-height: 1.5;
+        font-size: 1em;
+      } */
+
+      .channel-container {
+        margin-left: 16px;
+        max-width: 96%;
+        display: flex;
+        flex-direction: row;
+        flex-grow: 1;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        overflow-y: auto;
+      }
+
+      .player-container {
+        margin: 32px 0 0 16px;
+        border-radius: 8px;
+        display: inline-flex;
+      }
+
+      .player {
+        width: 900px;
+        height: 500px;
+        border-radius: 8px;
+      }
+
+      .discord {
+        margin-left: 32px;
+        width: 450px;
+      }
+
+      .discord widgetbot {
+        display: inline-block;
+        overflow: hidden;
+        background-color: rgb(54, 57, 62);
+        border-radius: 8px;
+        vertical-align: top;
+        width: 100%;
+        height: 100%;
+      }
+
+      .discord iframe {
+        border: none;
+        width: 100%;
+        height: 100%;
       }
       `
     ];
@@ -39,38 +122,89 @@ export class TvApp extends LitElement {
   // LitElement rendering template of your element
   render() {
     return html`
-      <h2>${this.name}</h2>
-      ${
-        this.listings.map(
-          (item) => html`
-            <tv-channel 
-              title="${item.title}"
-              presenter="${item.metadata.author}"
-              @click="${this.itemClick}"
-            >
-            </tv-channel>
-          `
-        )
-      }
-      <div>
+      <header>
+        <h1>${this.name}</h1>
+      </header>
+      <div class="channel-container">
+        ${
+          this.listings.map(
+            (item) => html`
+              <tv-channel 
+                id="${item.id}"
+                title="${item.title}"
+                presenter="${item.metadata.author}"
+                description="${item.metadata.description}"
+                video="${item.metadata.source}"
+                @click="${this.itemClick}">
+              </tv-channel>
+            `
+          )
+        }
+      </div>
+
+      <div class="player-container">
         <!-- video -->
+        <iframe class="player"
+          src="${this.createSource()}"
+          frameborder="0"
+          allowfullscreen>
+        </iframe>
         <!-- discord / chat - optional -->
+        <div class="discord">
+          <widgetbot server="954008116800938044" channel="1106691466274803723" width="100%" height="100%"><iframe title="WidgetBot Discord chat embed" allow="clipboard-write; fullscreen" src="https://e.widgetbot.io/channels/954008116800938044/1106691466274803723?api=a45a80a7-e7cf-4a79-8414-49ca31324752"></iframe></widgetbot>
+          <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+        </div>
+      </div>
       </div>
       <!-- dialog -->
-      <sl-dialog label="Dialog" class="dialog">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
+      <sl-dialog label=" ${this.activeItem.title}" class="dialog">
+        <p class="dialog-description">
+          ${this.activeItem.author}
+          ${this.activeItem.description}
+        </p>
+        <sl-button slot="footer" variant="primary" @click="${this.watchVideo}">Watch</sl-button>
       </sl-dialog>
     `;
   }
 
-  closeDialog(e) {
+  changeVideo() {
+    // Update the iframe source URL when an item is clicked
+    const iframe = this.shadowRoot.querySelector('iframe');
+    iframe.src = this.createSource();
+  }
+  
+  extractVideoID(link) {
+    try {
+      const url = new URL(link);
+      const searchParams = new URLSearchParams(url.search);
+      return searchParams.get("v");    
+    } catch (error) {
+      console.error("Invalid URL:", link);
+      return null;
+    }
+  }
+
+  createSource() {
+    return "https://www.youtube.com/embed/" + this.extractVideoID(this.activeItem.video);
+  }
+
+  closeDialog() {
     const dialog = this.shadowRoot.querySelector('.dialog');
     dialog.hide();
   }
 
+  watchVideo() {
+    this.changeVideo(); 
+  }
+
   itemClick(e) {
-    console.log(e.target);
+    this.activeItem = {
+      id: e.target.id,
+      title: e.target.title,
+      presenter: e.target.presenter,
+      description: e.target.description,
+      video: e.target.video
+    };
     const dialog = this.shadowRoot.querySelector('.dialog');
     dialog.show();
   }
@@ -95,5 +229,6 @@ export class TvApp extends LitElement {
     });
   }
 }
+
 // tell the browser about our tag and class it should run when it sees it
 customElements.define(TvApp.tag, TvApp);
--- src/tv-channel.js
+++ src/tv-channel.js
@@ -16,7 +16,9 @@ export class TvChannel extends LitElement {
   static get properties() {
     return {
       title: { type: String },
+      description: { type: String },
       presenter: { type: String },
+      video: { type: String }
     };
   }
   // LitElement convention for applying styles JUST to our element
@@ -25,18 +27,39 @@ export class TvChannel extends LitElement {
       :host {
         display: inline-flex;
       }
+
       .wrapper {
-        padding: 16px;
-        background-color: #eeeeee;
+        line-height: 4px;
+        min-width: 256px;
+        margin-right: 4px;
+        padding-left: 16px;
+        padding-right: 16px;
+        border: 1px solid #2c2c2c;
+        border-radius: 8px;
       }
+
+      .wrapper h5 {
+        font-size: 1.125em;
+        margin-bottom: 0.8888em;
+        color: #363636;
+      }
+      
+      p {
+        font-size: 1em;
+      }
+
+      ::slotted(p) {
+        line-height: 32px;
+      }
+
     `;
   }
   // LitElement rendering template of your element
   render() {
     return html`
       <div class="wrapper">
-        <h3>${this.title}</h3>
-        <h4>${this.presenter}</h4>
+        <h5>${this.title}</h5>
+        <p>${this.presenter}</p>
         <slot></slot>
       </div>  
       `;
